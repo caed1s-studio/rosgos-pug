645f8bcbbe12ded3394ce0e9b1d4aa911382ad6b
shchennikov <shchennikov@yandex.ru>
2020-06-23 11:18:14 +0300

    124
    12412

---
+++ build/assets/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var emptyObject = function emptyObject() {
+  return 'ES6!';
+};
+
+emptyObject();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6WyJlbXB0eU9iamVjdCJdLCJtYXBwaW5ncyI6Ijs7QUFDQSxJQUFNQSxXQUFXLEdBQUcsU0FBZEEsV0FBYztBQUFBLFNBQU0sTUFBTjtBQUFBLENBQXBCOztBQUVBQSxXQUFXIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxyXG5jb25zdCBlbXB0eU9iamVjdCA9ICgpID0+ICdFUzYhJztcclxuXHJcbmVtcHR5T2JqZWN0KCk7XHJcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
