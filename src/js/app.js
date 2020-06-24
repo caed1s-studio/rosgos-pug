1b6b64155dfc09badaf43ca8ac8d765382945666
shchennikov <shchennikov@yandex.ru>
2020-06-24 13:19:38 +0300

    87589

--- src/js/app.js
+++ src/js/app.js
@@ -21,45 +21,26 @@ if ($(window).width() > "1050") {
   });
 }
 
+// easyMain
+const easyMain = (num) => {
+  $(".easyMain__section1__items").slick({
+    infinite: true,
+    slidesToShow: num,
+    cssEase: "linear",
+    autoplaySpeed: 0,
+    arrows: false,
+    dots: true,
+  });
+};
 if ($(window).width() < "1050" && $(window).width() >= "770") {
-  $(".easyMain__section1__items").slick({
-    infinite: true,
-    slidesToShow: 3,
-    cssEase: "linear",
-    autoplaySpeed: 0,
-    arrows: false,
-    dots: true,
-  });
+  easyMain(3);
 } else if ($(window).width() < "770" && $(window).width() >= "575") {
-  $(".easyMain__section1__items").slick({
-    infinite: true,
-    slidesToShow: 2,
-    cssEase: "linear",
-    autoplaySpeed: 0,
-    arrows: false,
-    dots: true,
-  });
+  easyMain(2);
 } else if ($(window).width() < "575") {
-  $(".easyMain__section1__items").slick({
-    infinite: true,
-    slidesToShow: 1,
-    cssEase: "linear",
-    autoplaySpeed: 0,
-    arrows: false,
-    dots: true,
-  });
+  easyMain(1);
 }
 
-if ($(window).width() < "1050" && $(window).width() >= "770") {
-  $(".cards__inner").slick({
-    infinite: true,
-    slidesToShow: 3,
-    cssEase: "linear",
-    autoplaySpeed: 0,
-    arrows: false,
-    dots: true,
-  });
-} else if ($(window).width() < "770") {
+if ($(window).width() < "1050") {
   $(".cards__inner").slick({
     infinite: true,
     slidesToShow: 3,
@@ -78,27 +59,28 @@ $(".js-tab-trigger-questionsMain").click(function () {
   );
 });
 
-// if ($(window).width() > "768") {
-//   $(".educationSlider__content__spikers").slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     cssEase: "linear",
-//     autoplaySpeed: 0,
-//     appendArrows: $(".question__route"),
-//     prevArrow: $(".educationSlider__content__arrow__left"),
-//     nextArrow: $(".educationSlider__content__arrow__right"),
-//   });
-// } else {
-//   $(".educationSlider__content__spikers").slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoplay: false,
-//     autoplaySpeed: 3000,
-//     appendArrows: $(".question__route"),
-//     prevArrow: $(".educationSlider__content__arrow__left"),
-//     nextArrow: $(".educationSlider__content__arrow__right"),
-//   });
-// }
+$(".scopeMain__content__second__text").click(function () {
+  $(this).addClass("active");
+  $(".scopeMain__input").addClass("active");
+});
+
+$(".scopeMain__content__btn").click(function () {
+  $.ajax({
+    method: "POST",
+    url: "https://portal2.agentology.ru/api/agentology/",
+    dataType: "json",
+    data: {
+      key: "a9fb7d0b-d818-4e24-d499-dda5cb02dc6f",
+      operation: "user.registration",
+      data: JSON.stringify({
+        login: $(".scopeMain__content__input").val(),
+        referer: $(".scopeMain__input").val(),
+        type: "physical",
+      }),
+    },
+  }).done(function (msg) {
+    console.log("Sample of data:", msg);
+  });
+});
 
 emptyObject();
